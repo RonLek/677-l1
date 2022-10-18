dc1f48531021c005088562a27d554fa366b13376
Rohan Lekhwani <rohanlekhwani@ymail.com>
2022-10-18 12:03:13 -0400

    feat: add threading for multiple buyers and sellers

--- src/peer.py
+++ src/peer.py
@@ -3,9 +3,11 @@ import Pyro5.server
 import Pyro5.api
 from concurrent.futures import ThreadPoolExecutor
 from threading import Thread, Lock
+import datetime
 import time
 import random
 import re
+import sys
 
 class Peer(Thread):
 
@@ -29,10 +31,10 @@ class Peer(Thread):
     def get_random_neighbors(self):
         neighbor_list = []
         ns_dict = self.ns.list()
-        re_pattern = "seller[0-9]+@.|buyer[0-9]+@."
+        re_pattern = "seller[0-9]+|buyer[0-9]+"
         # print("ns_dict", ns_dict)
         for id in ns_dict:
-            if "NameServer" not in id and self.id != id and re.match(re_pattern, id) and self.hostname in id:
+            if "NameServer" not in id and self.id != id and re.match(re_pattern, id):
                 neighbor_list.append(id)
         self.connect_neighbors(neighbor_list)
 
@@ -43,15 +45,17 @@ class Peer(Thread):
     def connect_neighbors(self, neighbor_list):
 
         # randomly pick one neighbor
+        # print("neighbor list", neighbor_list)
         if neighbor_list:
-            random_neighbor_id = neighbor_list[random.randint(0, len(neighbor_list)-1)]
-            self.neighbors[random_neighbor_id] = self.ns.lookup(random_neighbor_id)
+            for i in range(3):
+                random_neighbor_id = neighbor_list[random.randint(0, len(neighbor_list)-1)]
+                self.neighbors[random_neighbor_id] = self.ns.lookup(random_neighbor_id)
 
             with Pyro5.api.Proxy(self.neighbors[random_neighbor_id]) as neighbor:
                 try:
                     self.executor.submit(neighbor.add_neighbor, self.id)
                 except Exception as e:
-                    print("Exception in connect_neighbors", e)
+                    print(datetime.datetime.now(), "Exception in connect_neighbors", e)
 
     @Pyro5.server.expose
     def add_neighbor(self, neighbor_id):
@@ -61,7 +65,7 @@ class Peer(Thread):
     def get_nameserver(self, ns_name):
 
         try:
-            ns = Pyro5.core.locate_ns()
+            ns = Pyro5.core.locate_ns(host=ns_name)
             return ns
         except Exception as e:
             template = "An exception of type {0} occurred at get_nameserver. Arguments:\n{1!r}"
@@ -77,42 +81,45 @@ class Peer(Thread):
                 self.ns._pyroClaimOwnership()
                 self.ns.register(self.id, uri)
                 if self.role == "buyer":
-                    print(time.time(), self.id, "joins to buy ", self.product_name)
+                    print(datetime.datetime.now(), self.id, "joins to buy ", self.product_name)
+                    time.sleep(1)
                 else:
-                    print(time.time(), self.id, "joins to sell ", self.product_name)
+                    print(datetime.datetime.now(), self.id, "joins to sell ", self.product_name)
+                    time.sleep(1)
 
                 self.executor.submit(daemon.requestLoop)
                 self.get_random_neighbors()
 
                 while True and self.role == "buyer":
 
-                    lookup_requests = []
+                    # lookup_requests = []
 
-                    print(self.neighbors, "\n")
+                    # print(self.neighbors, "\n")
                     for neighbor_name in self.neighbors:
                         with Pyro5.api.Proxy(self.neighbors[neighbor_name]) as neighbor:
                             search_path = [self.id]
-                            print(time.time() , self.id, "searching for ", self.product_name, " in ", neighbor_name)
-                            print("neighbor", neighbor)
+                            print(datetime.datetime.now() , self.id, "searching for ", self.product_name, " in ", neighbor_name)
+                            # print("neighbor", neighbor)
                             # neighbor._pyroClaimOwnership()
-                            lookup_requests.append(self.executor.submit(neighbor.lookup, self.id, self.product_name, 5, search_path, neighbor))
-                            print("lookup_requests", lookup_requests)
+                            neighbor.lookup(self.id, self.product_name, 5, search_path)
+                            # print("lookup_requests", lookup_requests)
 
-                    for request in lookup_requests:
+                    # for request in lookup_requests:
 
-                        request.result()
+                    #     request.result()
 
                     with self.seller_list_lock:
+
+                        print(datetime.datetime.now(), "seller list for ", self.id , "is: ", self.seller_list)
                         # select random seller
                         if self.seller_list:
                             random_seller = self.seller_list[random.randint(0, len(self.seller_list)-1)]
 
-                            with Pyro5.client.Proxy("PYRONAME:"+random_seller) as seller:
-                                future = self.executor.submit(seller.buy, self.id)
-                                if future.result():
-                                    print(time.time(), self.id, "bought", self.product_name, "from", random_seller)
-                                else:
-                                    print(time.time(), self.id, "failed to buy", self.product_name, "from", random_seller)
+                            with Pyro5.client.Proxy(self.neighbors[random_seller]) as seller:
+                                seller._pyroClaimOwnership()
+                                seller.buy(self.id)
+                        else:
+                            print(datetime.datetime.now(), "no sellers found within hop limit for ", self.id)
                         
                         self.seller_list = []
                         self.product_name = self.products[random.randint(0, len(self.products)-1)]
@@ -128,15 +135,16 @@ class Peer(Thread):
                             
 
     @Pyro5.server.expose
-    def lookup(self, bID, product_name, hopcount, search_path, neighbor):
+    def lookup(self, bID, product_name, hopcount, search_path):
         # this procedure, executed initially by buyer processes then recursively 
         # between directly connected peer processes in the network, should search 
         # the network; all matching sellers respond to this message with their IDs. 
         # The hopcount, which should be set lower than the maximum distance between peers in the network, 
         # is decremented at each hop and the message is discarded when it reaches 0.
         
-        neighbor._pyroClaimOwnership()
-        print("within lookup")
+        last_peer = search_path[-1]
+        # print(self.id, "\nneighbors")
+        # print(self.neighbors)        
         hopcount -= 1
 
         if hopcount < 0:
@@ -144,45 +152,57 @@ class Peer(Thread):
 
         try:
             if self.role == "seller" and self.product_name == product_name and self.product_count > 0:
-                print("Seller found with ID: ", self.id)
+                print(datetime.datetime.now(), "seller found with ID: ", self.id)
                 # inserting seller id at the front of the search path for easier reply
                 search_path.insert(0, self.id)
                 self.executor.submit(self.reply, self.id, search_path)
-                
+
             else:
                 # continue lookup
-                print("within continue lookup")
-                for neighbor_name in self.neighbors and neighbor_name not in search_path:
-                    with Pyro5.api.Proxy("PYRONAME:" + neighbor_name + "@localhost") as neighbor:
-                        self.executor.submit(neighbor.lookup, bID, product_name, hopcount, search_path)
+                # print("within continue lookup")
+                for neighbor_name in self.neighbors:
+                    if neighbor_name != last_peer:
+                        with Pyro5.api.Proxy(self.neighbors[neighbor_name]) as neighbor:
+                            # print("neighbor within continue lookup", neighbor)
+                            if self.id not in search_path:
+                                search_path.append(self.id)
+                            self.executor.submit(neighbor.lookup, bID, product_name, hopcount, search_path)
 
         except Exception as e:
-            print(e)
+            print(datetime.datetime.now(), "Exception in lookup", e)
             return
 
     @Pyro5.server.expose
     # :peer_id: the id of the buyer
     def buy(self, peer_id):
-        # TODO: implement locking
-        if self.product_count > 0:
 
-            self.product_count -= 1
-            print(time.time(), peer_id, "bought", self.product_name, "from", self.id, self.n_items, "remain now")
-        
-        # if self.product_count == 0, pick random item to sell
-        else:
-            print(time.time(), peer_id, "failed to buy", self.product_name, "from", self.id, "no more items")
-            self.product_name = self.products[random.randint(0, len(self.products) - 1)]
-            self.product_count = self.n
-            print(time.time(), self.id , "now selling", self.product_name)
+        try:
+            with self.product_count_lock:
+                if self.product_count > 0:
+
+                    self.product_count -= 1
+                    print("*******\n", datetime.datetime.now(), peer_id, "bought", self.product_name, "from", self.id, self.product_count, "remain now", "\n*******")
+                    return True
+            
+                # if self.product_count == 0, pick random item to sell
+                else:
+                    print(datetime.datetime.now(), peer_id, "failed to buy", self.product_name, "from", self.id, "no more items")
+                    self.product_name = self.products[random.randint(0, len(self.products) - 1)]
+                    self.product_count = self.n
+                    print(datetime.datetime.now(), self.id , "now selling", self.product_name)
+                    return False
+        except Exception as e:
+            print(datetime.datetime.now(), "Exception in buy", e)
+            return
 
     @Pyro5.server.expose
     def reply(self, peer_id, reply_path):
 
+        # print("reply_path", reply_path)
         try:
             # only 1 peer id in reply_path which is the seller
             if reply_path and len(reply_path) == 1:
-                print(time.time(), "Seller", peer_id, "responded to buyer", self.id)
+                print(datetime.datetime.now(), "Seller", peer_id, "responded to buyer", self.id)
 
                 # adding seller to the list of sellers
                 with self.seller_list_lock:
@@ -190,11 +210,14 @@ class Peer(Thread):
 
             elif reply_path and len(reply_path) > 1:
                 intermediate_peer = reply_path.pop()
-
+                # print("self.neighbors", self.neighbors)
+                # print("intermediate_peer", intermediate_peer)
                 with Pyro5.api.Proxy("PYRONAME:" + intermediate_peer) as neighbor:
-                    self.executor.submit(neighbor.reply, peer_id, reply_path)
+                    neighbor.reply(peer_id, reply_path)
 
         except Exception as e:
-            print(e)
-            return
+            template = "An exception of type {0} occurred at Reply. Arguments:\n{1!r}"
+            message = template.format(type(e).__name__, e.args)
+            print(datetime.datetime.now(), message)
+            sys.exit()
 
